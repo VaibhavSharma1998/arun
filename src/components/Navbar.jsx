05c152c09ad0673fd5a855b06c2ab34902d67403
VaibhavSharma1998 <sharmavaibhav203@gmail.com>
2023-10-05 23:00:13 +0530

    completed navbar and added main photo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { AiTwotonePhone,AiFillInstagram,AiFillFacebook,AiFillTwitterSquare,AiFillLinkedin} from "react-icons/ai";
+import { Link } from 'react-router-dom';
+import topImg from './img/arun main img.jpeg'
+
+export const Navbar = () => {
+  return (
+    <div className='w-full bg-black text-white  items-center '>
+       <div className="flex w-full px-[10%] h-10 border-t-4 border-[#69ffd6]">
+        <div className='w-[50%] flex items-center cursor-pointer'>
+           <p className='mr-1 text-[#69ffd6]'>{<AiTwotonePhone/>} </p> 
+            <p>Team: 01712 960400</p>
+       
+        </div>
+        <div className='w-[50% flex items-center ml-[30%]'>
+            <a href='https://www.instagram.com/arundhawan22/ ' target='blank' className='mr-5 hover:text-[#69ffd6]'>{<AiFillInstagram size={25}/>}</a>
+            <a href='https://www.facebook.com/arundhawanbwf' target='blank' className='mr-5 hover:text-[#69ffd6]'>{<AiFillFacebook size={25}/>}</a>
+            <a href='https://twitter.com/i/flow/login?redirect_after_login=%2FArunDhawan_' target='blank' className='mr-5 hover:text-[#69ffd6]'>{<AiFillTwitterSquare size={25}/>}</a>
+            <a href='https://www.linkedin.com/in/arun-dhawan/' target='blank' className='mr-5 hover:text-[#69ffd6]'>{<AiFillLinkedin size={25}/>}</a>
+        </div>
+       </div>
+       <div className=' bg-[#69ffd6]'>
+        <nav>
+          <ul className='flex items-center w-full  h-20'>
+            <div className='w-[50%] px-[10%]'>
+            <li>
+              <Link to='/home'>
+                <img src='https://bestwayfuture.com/data/logo.png' alt='logo' className='w-24 h-auto'/>
+              </Link>
+            </li>
+            </div>
+            <div className='w-[50%] flex items-center ml-[15%] text-xl font-semibold  text-black'>
+            <li className='mr-10 hover:text-[gray]'>
+              <Link to='/home'>Home</Link>
+            </li>
+            <li className='mr-10 hover:text-[gray]'> 
+              <Link to='/about '>About</Link>
+            </li>
+            <li className='mr-10 hover:text-[gray]'>
+              <Link to='/events'>Events</Link>
+            </li>
+            <li className='mr-10 hover:text-[gray]'>
+              <Link to='/contact'>Contact us</Link>
+            </li>
+            </div>
+          
+          </ul>
+        </nav>
+       </div>
+     <div className="relative">
+      <img src={topImg }alt='Arun Dhawan- MD'></img>
+      <p className='absolute top-[20%] right-[10%] text-7xl text-[#69ffd6] font-semibold'>Arun Dhawan</p>
+      <p className='absolute top-[35%] right-[18%] text-3xl'>Managing Director</p>
+      <p className='absolute top-[45%] right-[15%] text-5xl'>BestWay Future</p>
+     </div>
+    </div>
+  )
+}
---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div className='bg-red-600 text-black'>
+    <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga, ipsum impedit? Recusandae beatae cupiditate rerum officiis consequatur, eum veniam!</h1>  
+      </div>
+  )
+}
