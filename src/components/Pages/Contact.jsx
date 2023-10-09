4ab861447c4c824c336858aa3e3e8c9b477a1b3f
VaibhavSharma1998 <sharmavaibhav203@gmail.com>
2023-10-09 18:28:36 +0530

    "started contact page"

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import contactImg from '../img/contactImg.jpeg'
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='w-full bg-black text-white'>
+      <img src={contactImg} alt="Contact Img "  className='w-full'/>
+    </div>
   )
 }
